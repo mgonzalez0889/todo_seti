524312281d0b15c2a69b073fb895837b007b1829
Mijail Gonzalez <mijail0824@gmail.com>
2026-06-18 21:23:56 -0500

    settings styles and todo list color

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonToggle, IonIcon, IonList, IonListHeader, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonToggle, IonIcon, IonList, IonListHeader, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle } from '@ionic/angular/standalone';
 import { ThemeService } from '../services/theme.service';
 import { addIcons } from 'ionicons';
 import { moonOutline } from 'ionicons/icons';
@@ -10,7 +10,7 @@ import { RemoteConfigService } from '../services/remote-config.service';
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonListHeader, IonList, IonIcon, IonToggle, IonLabel, IonItem, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonCardSubtitle, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonListHeader, IonList, IonIcon, IonToggle, IonLabel, IonItem, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class SettingsPage {
   private themeService = inject(ThemeService);
